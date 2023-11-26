227c670c15cf0ede8c14b145681cbf36c699d2d7
Joao Allisson da Costa Silva <Joao.AllissondaCostaSilva@thomsonreuters.com>
2023-11-26 12:07:01 -0300

    Configure bootstrap. App yarn.lock

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,19 @@
+'use client'
+
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
 export default function Page() {
-    return <h1>Hello, Home pddddddage!</h1>
-  }
+  return (
+    <Card style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the s content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+  );
+}
--- src/api/api.ts
+++ src/api/api.ts
@@ -2,7 +2,6 @@ import axios from "axios";
 import { IUser } from "../models/user.model";
 import { IRepository } from "../models/repository.model";
 
-
 export async function getUser(user: string): Promise<IUser> {
     return axios.get<IUser>(`${process.env.NEXT_PUBLIC_GIT_API}/users/${user}`)
         .then(({ data }) => data );
@@ -15,5 +14,5 @@ export async function getRepository(data: { user: string; id: string }): Promise
 
 export async function getRepositoryList(userId: string): Promise<IRepository[]> {
     return axios.get<IRepository[]>(`${process.env.NEXT_PUBLIC_GIT_API}/users/${userId}/repos`)
-    .then(({ data }) => data );
+        .then(({ data }) => data );
 }
