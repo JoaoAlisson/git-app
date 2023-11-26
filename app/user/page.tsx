69b5ae8ce9cfe075d6c24c3f58fcbf3ef9331be2
Joao Allisson da Costa Silva <Joao.AllissondaCostaSilva@thomsonreuters.com>
2023-11-26 15:50:29 -0300

    user search page styled

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -1,51 +1,122 @@
-'use client'
+'use client';
 
 import { useState } from "react";
-import { useFormState, useFormStatus } from "react-dom"
-import Link from 'next/link'
+import Spinner from 'react-bootstrap/Spinner';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Form from 'react-bootstrap/Form';
+import Stack from 'react-bootstrap/Stack';
+import Alert from 'react-bootstrap/Alert';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Link from 'next/link';
 import { IUser } from "../../src/models/user.model";
 import { getUser } from "../../src/api/api";
 
-function searchUser(setUser: (user: any) => void) {
-  return async (_: any, formData: FormData) => {
-    const userName = formData.get('user') as string;
+const emptyUser: IUser | null = null;
 
-    if(!userName || userName.trim() === '') return;
+export default function Page() {  
+    const [user, setUser] = useState(emptyUser);
+    const [submited, setSubmited] = useState(false);
+    const [userName, setUserName] = useState('');
+    const [pending, setPending] = useState(false);
+    const [validated, setValidated] = useState(false);
 
-    try {
-      const user: IUser = await getUser(userName);
-      setUser(user);
-    } catch {
-      setUser(null);
+    function searchUser(): void { 
+      setPending(true);
+  
+      getUser(userName)
+        .then(user => setUser(user))
+        .catch(() => setUser(emptyUser))
+        .finally(() => setPending(false));
     }
-  }
-}
 
-export default function Page() {
-    const [user, setUser] = useState({} as IUser);
-    const [_, formAction] = useFormState(searchUser(setUser), null)
-    const { pending } = useFormStatus()
+    function handleSubmit(event: any): void {
+      const form = event.currentTarget;
+      const valid = form.checkValidity();
 
-    return <>
-      <h1>Buscar usuário Github</h1>
-      <form action={formAction}>
-        <input type="text" name="user" disabled={pending} />
-        <button type="submit" aria-disabled={pending}>
-          Submeter
-        </button>
-      </form>
-      <div>
-        { user?.id 
-          ? <div>
-              Usuário encontrado:
-              {user.name}
-              <Link href={`/user/view/${user.login}`}>Detalhes do Usuário</Link>
-              <Link href={`/repository/list/${user.login}`}>Repositórios do Usuário</Link>
-            </div>
-          : <div>
-              Usuário não encontrado
-            </div>
-        }
-      </div>
-    </>
+      event.preventDefault();
+      event.stopPropagation();
+      
+      if(valid) {      
+        setSubmited(true);
+        searchUser();
+      }
+      
+      setValidated(true);
+    };
+
+    return <Stack gap={3}>
+        <Container>
+          <h1>Buscar usuário Github</h1>
+        </Container>
+        <Container>
+          <Form noValidate validated={validated} onSubmit={handleSubmit}>
+            <Row>
+              <Col md={5}>
+                <Form.Group controlId="validationCustomUser">
+                  <InputGroup hasValidation>
+                    <Form.Control 
+                      type="text" 
+                      name="user" 
+                      disabled={pending} 
+                      required
+                      placeholder="Usuário"
+                      onChange={(event) => {
+                        setUserName(event.target.value);
+                      }}
+                       />
+                      <Form.Control.Feedback type="invalid">
+                        Compo obrigatório
+                      </Form.Control.Feedback>
+                  </InputGroup>
+                </Form.Group>
+              </Col>
+              <Col sm={12} md={3}>
+                <Button 
+                  type="submit" 
+                  variant="success" 
+                  aria-disabled={pending}>
+                    { pending && 
+                        <Spinner
+                          as="span"
+                          animation="grow"
+                          size="sm"
+                          role="status"
+                          aria-hidden="true"
+                        />
+                    }
+                    { pending ? 'Pesquisando...' : 'Pesquisar' }
+                </Button>
+              </Col>
+            </Row>
+          </Form >
+        </Container>
+
+        {submited && !pending &&
+          <Container>
+            <Row>
+              <Col>
+                { user?.id 
+                  ? <Card className="col-md-4">
+                      <Card.Img variant="top" src={user.avatar_url} />
+                      <Card.Body>
+                        <Card.Title>{user.name}</Card.Title>
+                        <Card.Text>
+                          <p>
+                            <b>Bio: </b> {user.bio}
+                          </p>
+                          <Link className="" href={`/repository/list/${user.login}`}>Repositórios do Usuário</Link>
+                        </Card.Text>
+                        <Link className="btn btn-primary" href={`/user/view/${user.login}`}>Detalhes do Usuário</Link>
+                      </Card.Body>
+                    </Card>        
+                  : <Alert variant="warning">Usuário não encontrado</Alert>
+                }
+              </Col>
+            </Row>
+        </Container>}
+    </Stack>
   }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,13 +6,13 @@ import Navbar from 'react-bootstrap/Navbar';
 
 export default function Header() {
     return (
-        <Navbar expand="lg" className="bg-body-tertiary">
+        <Navbar expand="lg" className="bg-body-tertiary" bg="dark" data-bs-theme="dark">
             <Container>
-                <Navbar.Brand href="/">GIT API</Navbar.Brand>
+                <Navbar.Brand href="/">Github API</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
-                    <Nav.Link href="/home">Home</Nav.Link>
+                    <Nav.Link href="/">Home</Nav.Link>
                     <Nav.Link href="/user">Pesquisar Usuário</Nav.Link>
                 </Nav>
                 </Navbar.Collapse>
